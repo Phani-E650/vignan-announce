3987dac3147592a44ab1a5dcfb5e052fa16e9288
U-C-S <ucs3721@outlook.com>
2021-04-06 20:02:58 +0530

    More code management

--- Scripts/common.js
+++ Scripts/common.js
@@ -1,10 +1,1 @@
-console.log("Created by @Phani-E650 and @U-C-S");
-
-/*
-let car = document.querySelector(".carousel").getElementsByTagName("div");
-for (let i = 0; i < car.length; i++) {
-  const element = car[i];
-  set
-  
-}
-*/
+console.log("Created by @Phani-E650");
